3855de0938b6d602adced5da097551182ac47fe2
James Johnson <johnsojr@gmail.com>
2014-12-01 18:27:30 -0600

    Revert ad11027..b3e5b8e
    This rolls back to commit ad11027513403dbaf011cfb078f715d32337cc05.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,12 @@
-console.log('\'Allo \'Allo!');
+/* main.js
+ * http://mcadsustainabledesign.com
+ */
+
+;(function($, window, document, undefined) {
+  'use strict';
+
+  $(document).ready(function(){
+    $('.article-block').imagefill();
+  }); // end .ready()
+
+})(jQuery, window, document);
